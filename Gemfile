007246fbc509e19d7999529b105940d91928ae53
scylla <hasijaadarsh@gmail.com>
2012-04-28 00:05:11 -0500

    rhoconnect changed

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'pg'
 
 gem 'json'
 
+gem 'rhoconnect-rb'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
+	include Rhoconnect::Resource
+	def partition
+		"adam"
+	end
+	def self.rhoconnect_query(partition)
+		Product.all
+	end
   attr_accessible :brand, :name, :price, :quantity, :sku
 end
